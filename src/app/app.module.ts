57b8d79f2fb1e147aea31590451030025430ff19
Geraldine <geraldine@g-angle.co.jp>
2017-02-20 14:16:11 +0800

    Update angular cli version for local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { HeaderComponent } from "./shared/header.component";
+import { SigninComponent } from "./unprotected/signin.component";
+import { SignupComponent } from "./unprotected/signup.component";
+import { ProtectedComponent } from "./protected/protected.component";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        SigninComponent,
+        SignupComponent,
+        ProtectedComponent
+    ],
+    imports: [BrowserModule, HttpModule, ReactiveFormsModule],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
